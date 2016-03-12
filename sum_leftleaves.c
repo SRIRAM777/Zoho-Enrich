92d498b2234a4127d795a9105fb24feb4c6b0baa
Vinith Kannan A <vinithias@gmail.com>
2016-03-12 10:49:07 +0530

    Update sum_leftleaves.c

--- sum_leftleaves.c
+++ sum_leftleaves.c
@@ -4,24 +4,25 @@
 struct binarytree
 {
     int data;
-    struct binarytree* left, *right;
+    struct binarytree *left, *right;
 };
 
 struct binarytree *insert(int num)
 {
-    struct binarytree *node=(struct binarytree *)malloc(sizeof(struct binarytree));
-    node->data=num;
-    node->right= node->left = NULL;
+    struct binarytree *node=malloc(sizeof(struct binarytree));
+    node->data  =   num;
+    node->right =   NULL;
+    node->left  =   NULL;
     return node;
 }
- 
+
 int check(struct binarytree *node)
 {
-   if (node==NULL)
-       return 0;
-   if ((node->left==NULL)&&(node->right==NULL))
-       return 1;
-   return 0;
+    if (node == NULL)
+        return 0;
+    if ((node->left == NULL)&&(node->right == NULL))
+        return 1;
+    return 0;
 }
 
 int sum(struct binarytree *root)
@@ -29,15 +30,17 @@ int sum(struct binarytree *root)
     int res=0;
     if (root != NULL)
     {
-       if (check(root->left))
-            res+=root->left->data;
-       else 
-            res+=sum(root->left);
-       res+=sum(root->right);
+        if (check(root->left))
+            res=res + root->left->data;
+        else
+            res=res + sum(root->left);
+        
+        res=res + sum(root->right);
     }
     return res;
 }
- 
+
+
 int main()
 {
     struct binarytree *root   = insert(54);
@@ -46,9 +49,9 @@ int main()
     root->right->left         = insert(20);
     root->right->right        = insert(10);
     root->right->right->left  = insert(25);
-    root->left->left          = insert(12);
-    root->left->right         = insert(30);
-    root->left->right->right  = insert(15);
+    root->left->right         = insert(12);
+    root->left->right->right  = insert(30);
+    root->left->left          = insert(10);
     printf("Sum of all left leaves in a binary tree = %d \n",sum(root));
     
     return 0;
